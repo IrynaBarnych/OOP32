5a5a7c759afc716e76a93363765d9371dc172a6e
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 20:20:59 +0200

    Завдання №1

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-a = 1
-print(type(a))
-print(type(type))
-class MyParentClass1: pass
-class MyParentClass2: pass
-def summa(self):
-    return  sum(range(10))
-
-MyClass = type("MyClass", (MyParentClass1, MyParentClass2), {"x": 2, "y": 5,
-                                                             "my_metod": lambda self: "Це метод класу",
-                                                             "summa":summa})
-obj = MyClass()
-print(obj.x)
-print(obj.y)
-print(obj.my_metod())
-print(MyClass.__mro__)
-print(obj.summa())
-
+"""Завдання 1
+Задайте метаклас, що автоматично додає
+додатковий функціонал до всіх класів, що його
+використовують."""
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
-        #маніпуляція з класом
-        dct["attr"] = 100
-        print(name)
-        print(bases)
-        print(dct)
+        dct["hello"] = lambda self: print(f"Hello from {self.__class__.__name__}")
         return super().__new__(cls, name, bases, dct)
 
-class MyClass(metaclass=MyMeta):
+class MyClass(metaclass = MyMeta):
     attr = 10
     def metod(self):
         pass
+
 print(dir(MyClass))
 obj = MyClass()
 print(obj.attr)
+obj.hello()
