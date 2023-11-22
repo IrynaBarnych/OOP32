518d0cfbfa97bf3e927399adf41062a6b0768c47
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 19:57:33 +0200

    маніпуляція з атрибутами

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ print(obj.y)
 print(obj.my_metod())
 print(MyClass.__mro__)
 print(obj.summa())
+
+class MyMeta(type):
+    def __new__(cls, name, bases, dct):
+        #маніпуляція з класом
+        dct["attr"] = 100
+        print(name)
+        print(bases)
+        print(dct)
+        return super().__new__(cls, name, bases, dct)
+
+class MyClass(metaclass=MyMeta):
+    attr = 10
+    def metod(self):
+        pass
+print(dir(MyClass))
+obj = MyClass()
+print(obj.attr)
