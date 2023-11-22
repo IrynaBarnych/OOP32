409406e6ead59475ad603fc4a11c117a91716dcb
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 21:25:07 +0200

    + і -

--- main.py
+++ main.py
@@ -63,3 +63,45 @@ obj = MyClass()
 obj.calculate(5)
 obj.calculate(100)
 obj.calculate(120)
+
+###
+"""Зміна поведінки операторів класу: Створіть метаклас,
+який дозволяє змінювати поведінку різних операторів
+(наприклад, +, -, *, /) для об'єктів певного класу."""
+
+# +
+class OperatorOverloadMeta(type):
+    def __new__(cls, name, bases, dct):
+         dct["__add__"] = lambda self, other: self.add(other)
+         return super().__new__(cls, name, bases, dct)
+
+
+class MyClass(metaclass=OperatorOverloadMeta):
+    def __init__(self, value):
+        self.value = value
+
+    def add(self, other):
+        return  self.value + other.value if isinstance(other, MyClass) else self.value + other
+
+obj1 = MyClass(5)
+obj2 = MyClass(6)
+print(obj1 + obj2)
+
+
+# -
+class OperatorOverloadMeta(type):
+    def __new__(cls, name, bases, dct):
+         dct["__sub__"] = lambda self, other: self.add(other)
+         return super().__new__(cls, name, bases, dct)
+
+
+class MyClass(metaclass=OperatorOverloadMeta):
+    def __init__(self, value):
+        self.value = value
+
+    def add(self, other):
+        return self.value - other.value if isinstance(other, MyClass) else self.value + other
+
+obj1 = MyClass(6)
+obj2 = MyClass(5)
+print(obj1 - obj2)
