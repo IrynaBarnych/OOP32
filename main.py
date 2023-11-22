6b9eb25d54d131cacfd81b2e087c9cb899d75ae2
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 20:39:46 +0200

    Завдання №2

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-"""Завдання 2"""
+"""Завдання 2
+Створіть метаклас, що перевіряє наявність певних
+атрибутів у всіх класах, які використовують цей
+метаклас."""
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
         #перелік атрибутів
         required_attributes = ['attr1', "attr2"]
-        for i in dct:
-            raise AttributeError(f"В класі {name} повинен бути тільки {i} атрибут")
+        for i in required_attributes:
+            if i not in dct:
+                raise AttributeError("В класі повинен мати тільки ['attr1', 'attr2'] атрибути")
         return super().__new__(cls, name, bases, dct)
 
 class MyClass(metaclass = MyMeta):
     attr1 = 10
     attr2 = 100
-    attr3 = "Hello!"
+    #attr3 = "Hello!"
 
 print(dir(MyClass))
 obj = MyClass()
-print(obj.attr)
-obj.hello()
+print(obj.attr1)
