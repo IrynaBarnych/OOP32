33a66949014bd07cc8c4a7a2f622c8a753952c75
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 19:37:39 +0200

    динамічне створення класу

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ print(type(a))
 print(type(type))
 class MyParentClass1: pass
 class MyParentClass2: pass
-MyClass = type("MyClass", (MyParentClass1, MyParentClass2), {"x": 2, "y": 5, "my_metod": lambda self: "Це метод класу"})
+def summa(self):
+    return  sum(range(10))
+
+MyClass = type("MyClass", (MyParentClass1, MyParentClass2), {"x": 2, "y": 5,
+                                                             "my_metod": lambda self: "Це метод класу",
+                                                             "summa":summa})
 obj = MyClass()
 print(obj.x)
 print(obj.y)
 print(obj.my_metod())
 print(MyClass.__mro__)
+print(obj.summa())
