007d2a6e4855efb62e3a98748fb945f90dc7c9e3
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-22 21:08:14 +0200

    Метаклас

--- main.py
+++ main.py
@@ -32,3 +32,34 @@ class MyClass(metaclass=MyMeta):
 print(dir(MyClass))
 obj = MyClass()
 print(obj.attr)
+
+###кешування
+"""Метаклас для кешування результатів методів класу:
+ Реалізуйте метаклас, який автоматично кешує
+ результати виклику методів класу для підвищення швидкодії."""
+
+
+class Cached(type):
+    def __new__(cls, name, bases, dct):
+        cache = {}
+
+        def wrap_method(method):
+            def wrapped(*args, **kwargs):
+                cache_key = (method.__name__, args, frozenset(kwargs.items()))
+                if cache_key not in cache:
+                    cache[cache_key] = method(*args, **kwargs)
+                print(cache)
+            return wrapped
+        for attr_name, attr_value in dct.items():
+            if callable(attr_value):
+                dct[attr_name] = wrap_method(attr_value)
+        return super().__new__(cls, name, bases, dct)
+
+
+class MyClass(metaclass=Cached):
+    def calculate(self, x):
+        return print(f"Розрахунок для {x} - {x ** 2}")
+obj = MyClass()
+obj.calculate(5)
+obj.calculate(100)
+obj.calculate(120)
