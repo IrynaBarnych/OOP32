c6ad6f404e0de7a16765d8477b0d0483a2ef103c
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-24 23:40:41 +0200

    додала код

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-"""Завдання 2
-Створіть метаклас, що перевіряє наявність певних
-атрибутів у всіх класах, які використовують цей
-метаклас."""
+# Завдання 3
+# Реалізуйте метаклас, що забороняє спадкування від певних класів чи змінює порядок спадкування.
 
 
 class MyMeta(type):
@@ -14,7 +12,22 @@ class MyMeta(type):
             if i not in dct:
                 raise AttributeError(f"Клас повинен мати обов'язковий атрибут: {i}")
 
+        # кортеж заборонених класів
+        forbidden_classes = (ForbiddenBaseClass1, ForbiddenBaseClass2)
+
+        # перевіряємо, чи жоден з заборонених класів не є в списку базових класів
+        if any(forbidden_class in bases for forbidden_class in forbidden_classes):
+            raise TypeError(f"Заборонено спадкування від певних класів: {forbidden_classes}")
+
         return super().__new__(cls, name, bases, dct)
+
+
+class ForbiddenBaseClass1:
+    pass
+
+
+class ForbiddenBaseClass2:
+    pass
 
 
 class MyClass(metaclass=MyMeta):
@@ -22,7 +35,8 @@ class MyClass(metaclass=MyMeta):
     attr2 = 100
     # attr3 = "Hello!"
 
-print(
-    dir(MyClass))
+
+print(dir(MyClass))
 obj = MyClass()
 print(obj.attr1)
+
