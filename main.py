3186f6db6da110aff365544c7adb46666c4dbeeb
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-24 23:30:43 +0200

    додала код

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 Створіть метаклас, що перевіряє наявність певних
 атрибутів у всіх класах, які використовують цей
 метаклас."""
+
+
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
-        #перелік атрибутів
-        required_attributes = ['attr1', "attr2"]
+        # перелік атрибутів, які мають бути в класі
+        required_attributes = ['attr1', 'attr2']
+
+        # перевіряємо, чи всі обов'язкові атрибути присутні в dct
         for i in required_attributes:
             if i not in dct:
-                raise AttributeError("В класі повинен мати тільки ['attr1', 'attr2'] атрибути")
+                raise AttributeError(f"Клас повинен мати обов'язковий атрибут: {i}")
+
         return super().__new__(cls, name, bases, dct)
 
-class MyClass(metaclass = MyMeta):
+
+class MyClass(metaclass=MyMeta):
     attr1 = 10
     attr2 = 100
-    #attr3 = "Hello!"
+    # attr3 = "Hello!"
 
-print(dir(MyClass))
+print(
+    dir(MyClass))
 obj = MyClass()
 print(obj.attr1)
