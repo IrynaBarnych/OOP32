9e76ec524431f444d26416b9d0331de25f47326b
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-25 15:00:42 +0200

    додала код

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #Завдання 1
-#Метаклас, який вносить додаткові перевірки/логіку
-#до певних методів у всіх класах.
+# Метаклас, який вносить додаткові перевірки/логіку
+# до певних методів у всіх класах.
 
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
+        # Додаємо метод "hello" до класу
         dct["hello"] = lambda self: print(f"Hello from {self.__class__.__name__}")
         return super().__new__(cls, name, bases, dct)
 
-class MyClass(metaclass = MyMeta):
+
+class MyClass(metaclass=MyMeta):
     attr = 10
-    def metod(self):
+
+    def method(self):
         pass
 
+
+# Виправляємо помилку у назві методу
 print(dir(MyClass))
 obj = MyClass()
 print(obj.attr)
 obj.hello()
+
