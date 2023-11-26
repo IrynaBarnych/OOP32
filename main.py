1b5d898ed3cffbc35dd871644f3e2a256df398ce
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-26 20:42:02 +0200

    додала код

--- main.py
+++ main.py
@@ -4,10 +4,8 @@
 
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
-        # Додаємо метод "hello" до класу
         dct["hello"] = lambda self: print(f"Hello from {self.__class__.__name__}")
         return super().__new__(cls, name, bases, dct)
-
 
 class MyClass(metaclass=MyMeta):
     attr = 10
@@ -15,10 +13,9 @@ class MyClass(metaclass=MyMeta):
     def method(self):
         pass
 
-
-# Виправляємо помилку у назві методу
 print(dir(MyClass))
 obj = MyClass()
 print(obj.attr)
 obj.hello()
+
 
