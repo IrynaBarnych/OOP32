318e0402e583fad9007949243967e4cadc2c9cb1
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-02 22:25:02 +0200

    код відредагувала

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class Meta(type):
     def __new__(cls, name, bases, dct):
         for forbidden_class in cls.forbidden_classes:
             if forbidden_class in bases:
-                return TypeError(f"Спадкування від класу '{forbidden_class.__name__}' не дозволено.")
+                raise TypeError(f"Спадкування від класу '{forbidden_class.__name__}' не дозволено.")
+        return super().__new__(cls, name, bases, dct)
+
 class MyClass(metaclass=Meta):
     attr = 10
 
@@ -19,3 +21,6 @@ Meta.forbidden_classes.append(ForbiddenClass)
 
 if ForbiddenClass in Meta.forbidden_classes:
     print(f"Спадкування від класу '{ForbiddenClass.__name__}' не дозволено.")
+
+class AnotherClass(MyClass):
+    pass
